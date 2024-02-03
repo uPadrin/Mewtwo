28ed5fcf65cb1118bca72e58f8b5821440c9303a
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2024-02-03 10:18:57 -0300

    Person

---
+++ cardProcessor/src/main/java/com/mewtow/cardProcessor/models/Person.java
@@ -0,0 +1,23 @@
+package com.mewtow.cardProcessor.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "tb_person")
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private  String name;
+
+    @NotNull
+    private int age;
+}
