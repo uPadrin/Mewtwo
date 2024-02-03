a169531e9486f17485a219a5e463008393533a86
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2024-02-03 12:49:57 -0300

    ambiente de teste

--- cardProcessor/src/main/java/com/mewtow/cardProcessor/CardProcessorApplication.java
+++ cardProcessor/src/main/java/com/mewtow/cardProcessor/CardProcessorApplication.java
@@ -1,13 +1,30 @@
 package com.mewtow.cardProcessor;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 public class CardProcessorApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {/* {
 		SpringApplication.run(CardProcessorApplication.class, args);
+	} */
+			try (CSVReader reader = new CSVReader(new FileReader("C:\\Users\\bryan\\OneDrive\\Documentos\\Hackthon - AsapCard\\input-data.csv"))) {
+				List<String[]> r = reader.readAll();
+				r.forEach(x -> System.out.println(Arrays.toString(x)));
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException(e);
+			} catch (IOException | CsvException e) {
+				throw new RuntimeException(e);
+			}
+		}
 	}
 
-}
