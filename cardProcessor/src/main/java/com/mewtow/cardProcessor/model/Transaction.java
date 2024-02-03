0fa8e1b8bb114e7696894599b332f95b34020cb8
Vitor Ferreira <vitornasc673@gmail.com>
2024-02-03 11:27:04 -0300

    Modeling Transaction

--- cardProcessor/src/main/java/com/mewtow/cardProcessor/model/Transaction.java
+++ cardProcessor/src/main/java/com/mewtow/cardProcessor/model/Transaction.java
@@ -6,10 +6,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
-
-@Entity	
-@Table(name ="db_cardProcessor")
+@Entity
+@Getter
+@Setter
+@Table(name ="tb_transaction")
 public class Transaction {
 	
 	@Id
@@ -23,37 +26,4 @@ public class Transaction {
 
 	private int amount;
 	
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public Long getPerson_id() {
-		return person_id;
-	}
-
-	public void setPerson_id(Long person_id) {
-		this.person_id = person_id;
-	}
-
-	public String getTransaction_date() {
-		return transaction_date;
-	}
-
-	public void setTransaction_date(String transaction_date) {
-		this.transaction_date = transaction_date;
-	}
-
-	public int getAmount() {
-		return amount;
-	}
-
-	public void setAmount(int amount) {
-		this.amount = amount;
-	}
-
 }
