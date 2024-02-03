9c312127bc4004b50ea97857a20bbc594b85ec8d
Bryan S. Vieira <bryan.vieira2013@gmail.com>
2024-02-03 17:38:32 -0300

    Converçcao para jason

--- cardProcessor/src/main/java/com/mewtow/cardProcessor/CardProcessorApplication.java
+++ cardProcessor/src/main/java/com/mewtow/cardProcessor/CardProcessorApplication.java
@@ -1,56 +1,23 @@
 package com.mewtow.cardProcessor;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import com.opencsv.CSVReader;
-import com.opencsv.exceptions.CsvException;
-import com.opencsv.exceptions.CsvValidationException;
-import org.json.JSONArray;
-import org.json.JSONObject;
-import org.springframework.boot.SpringApplication;
+import com.mewtow.cardProcessor.Service.PersonService;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import org.json.JSONObject;
 
 @SpringBootApplication
 public class CardProcessorApplication {
 
-	public static void main(String[] args) {/* {
+
+    public static void main(String[] args) {/* {
 		SpringApplication.run(CardProcessorApplication.class, args);
 	} */
 
-        String csvFile = "C:\\Users\\bryan\\OneDrive\\Documentos\\Mewtwo\\cardProcessor\\input-data.csv";
-        String jsonFile = "DONE.json";
+        PersonService personService = new PersonService();
 
-        try {
-            BufferedReader br = new BufferedReader(new FileReader(csvFile));
-            String line;
-            JSONArray jsonArray = new JSONArray();
+        personService.leitor();
 
-            while ((line = br.readLine()) != null) {
-                String[] fields = line.split(",");
-                JSONObject jsonObject = new JSONObject();
-                for (int i = 0; i < fields.length; i++) {
-                    jsonObject.put("field" + i, fields[i]);
-                }
-                jsonArray.add(jsonObject);
-            }
-
-            FileWriter fileWriter = new FileWriter(jsonFile);
-            fileWriter.write(jsonArray.toJSONString());
-            fileWriter.flush();
-            fileWriter.close();
-
-            br.close();
-            System.out.println("Conversion completed: CSV to JSON");
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
     }
 }
