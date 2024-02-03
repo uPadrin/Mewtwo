af9f08270e85ac4c6c77104e303c3ac0bad1ae83
Leo <leonardo.r.s.duarte@gmail.com>
2024-02-03 09:26:47 -0300

    First commit

---
+++ cardProcessor/src/test/java/com/mewtow/cardProcessor/CardProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mewtow.cardProcessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
